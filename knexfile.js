9d0922f3c3f1035abda145c51dc3cc6d5de6fbed
unknown <Ismail@abderrazak.Atirao.ma>
2025-12-06 17:47:05 +0100

    adding databaseddssezz

--- knexfile.js
+++ knexfile.js
@@ -14,7 +14,7 @@ module.exports = {
     pool: { min: 2, max: 10 },
     migrations: {
       tableName: 'knex_migrations',
-      directory: './migrations'
+      directory: './db/migrations'
     },
     seeds: {
       directory: './seeds'
