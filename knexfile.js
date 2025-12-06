708c5b5f7e3b3234130e92a6323b7b66ab6c2162
unknown <Ismail@abderrazak.Atirao.ma>
2025-12-06 17:28:53 +0100

    adding databasedd

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,27 @@
 require('dotenv').config();
 
 module.exports = {
+
   development: {
+    client: 'mysql2',
+    connection: {
+      host: process.env.DB_HOST || '127.0.0.1',
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT || 3306
+    },
+    pool: { min: 2, max: 10 },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
+  },
+
+  production: {
     client: 'mysql2',
     connection: {
       host: process.env.DB_HOST,
@@ -10,16 +30,10 @@ module.exports = {
       database: process.env.DB_NAME,
       port: process.env.DB_PORT || 3306
     },
-    pool: {
-      min: 1,
-      max: 5
-    },
+    pool: { min: 2, max: 20 },
     migrations: {
       tableName: 'knex_migrations',
       directory: './migrations'
-    },
-    seeds: {
-      directory: './seeds'
     }
   }
 };
